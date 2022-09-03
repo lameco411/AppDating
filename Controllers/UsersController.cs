8e96b8fd9e7995d17a840e1e67f4e0ec19fa8431
Lâm Eco <lameco411@gmail.com>
2022-09-03 20:13:28 +0700

    first learning commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Controllers
+{
+     [ApiController]
+        [Route("api/[controller]")]
+       public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+        public UsersController (DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+           return  Json( await _context.Users.ToListAsync());
+        }
+         [HttpGet("{id}")]
+        public IActionResult GetUsers(int id)
+        {
+           return Json (_context.Users.FirstOrDefault(x=>x.Id==id));
+        }
+    }
+}
